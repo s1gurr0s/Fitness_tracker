f477d11ec68cdcefe02d448b9cfb738dfe107ad8
Владимир Агукин <vva89@yandex.ru>
2022-02-13 17:32:27 +0300

    ДЗ 1.2

--- homework.py
+++ homework.py
@@ -14,8 +14,8 @@ class InfoMessage:
 
     TRAINING_INFO = (
         'Тип тренировки: {}; Длительность: {:.3f} ч.; '
-        + 'Дистанция: {:.3f} км; Ср. скорость: {:.3f} км/ч; '
-        + 'Потрачено ккал: {:.3f}.'
+        'Дистанция: {:.3f} км; Ср. скорость: {:.3f} км/ч; '
+        'Потрачено ккал: {:.3f}.'
     )
 
     def get_message(self) -> str:
@@ -63,12 +63,15 @@ class Training:
 class Running(Training):
     """Тренировка: бег."""
 
-    MSP_MULTIPL = 18
-    MSP_SUBTR = 20
+    MEAN_SPEED_MULTIPLIER = 18
+    MEAN_SPEED_SHIFT = 20
 
     def get_spent_calories(self) -> float:
         return (
-            (self.MSP_MULTIPL * self.get_mean_speed() - self.MSP_SUBTR)
+            (
+                self.MEAN_SPEED_MULTIPLIER * self.get_mean_speed()
+                - self.MEAN_SPEED_SHIFT
+            )
             * self.weight / self.M_IN_KM * self.duration * self.MIN_IN_H
         )
 
@@ -77,15 +80,18 @@ class Running(Training):
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
 
-    WEIGHT_MULTIPL = 0.035
-    COEFF_FOR_SPENT_CAL = 0.029
+    WEIGHT_MULTIPLIER_1 = 0.035
+    WEIGHT_MULTIPLIER_2 = 0.029
 
     height: float
 
     def get_spent_calories(self) -> float:
         return (
-            (self.WEIGHT_MULTIPL * self.weight + (self.get_mean_speed() ** 2
-             // self.height) * self.COEFF_FOR_SPENT_CAL * self.weight)
+            (
+                self.WEIGHT_MULTIPLIER_1 * self.weight
+                + (self.get_mean_speed() ** 2 // self.height)
+                * self.WEIGHT_MULTIPLIER_2 * self.weight
+            )
             * self.duration * self.MIN_IN_H
         )
 
@@ -95,8 +101,8 @@ class Swimming(Training):
     """Тренировка: плавание."""
 
     LEN_STEP = 1.38
-    MSP_ADD = 1.1
-    COEFF_FOR_SPENT_CAL = 2
+    MEAN_SPEED_SHIFT = 1.1
+    WEIGHT_MULTIPLIER = 2
 
     length_pool: float
     count_pool: int
@@ -109,37 +115,40 @@ class Swimming(Training):
 
     def get_spent_calories(self) -> float:
         return (
-            (self.get_mean_speed() + self.MSP_ADD)
-            * self. COEFF_FOR_SPENT_CAL * self.weight
+            (self.get_mean_speed() + self.MEAN_SPEED_SHIFT)
+            * self. WEIGHT_MULTIPLIER * self.weight
         )
 
 
-SPORT_TYPE = {
+SPORTS_TYPE_DATA = {
     'SWM': (Swimming, len(fields(Swimming))),
     'RUN': (Running, len(fields(Running))),
     'WLK': (SportsWalking, len(fields(SportsWalking)))
 }
 
-EXCEPT_1 = (
+WRONG_WORKOUT_TYPE = 'Код тренировки {} не распознан'
+WRONG_NUMBER_OF_PARAMETERS = (
     'Для расчета результатов тренировки '
-    + 'класса {} передано неверное количество '
-    + 'параметров: {}. Необходимо {}'
+    'класса {} передано неверное количество '
+    'параметров: {}. Необходимо {}'
 )
-EXCEPT_2 = 'Код тренировки {} не распознан'
 
 
 def read_package(workout_type: str, data: Sequence[float]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    if workout_type in SPORT_TYPE:
-        if len(data) == SPORT_TYPE[workout_type][1]:
-            return SPORT_TYPE[workout_type][0](*data)
-        else:
-            raise ValueError(
-                EXCEPT_1.format(workout_type, len(data),
-                                SPORT_TYPE[workout_type][1])
-            )
+    if workout_type not in SPORTS_TYPE_DATA:
+        raise ValueError(WRONG_WORKOUT_TYPE.format(workout_type))
     else:
-        raise ValueError(EXCEPT_2.format(workout_type))
+        SPORTS_TYPE_DATA.get(workout_type)
+        if len(data) != SPORTS_TYPE_DATA.get(workout_type)[1]:
+            raise ValueError(
+                WRONG_NUMBER_OF_PARAMETERS.format(
+                    workout_type, len(data),
+                    SPORTS_TYPE_DATA[workout_type][1]
+                )
+            )
+        else:
+            return SPORTS_TYPE_DATA[workout_type][0](*data)
 
 
 def main(training: Training) -> None:
@@ -149,7 +158,7 @@ def main(training: Training) -> None:
 
 if __name__ == '__main__':
     packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
+        ('SWM', [720, 1, 80, 25, 40, 50]),
         ('RUN', [15000, 1, 75]),
         ('WLK', [9000, 1, 75, 180])
     ]
